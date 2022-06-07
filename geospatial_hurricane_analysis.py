f22edd8631bc4ca9868c3cf6e00e51b5cfe20325
Josh Patterson <jpatterson@floe.tv>
2022-06-07 16:26:52 -0400

    added basic map building functionality

--- geospatial_hurricane_analysis.py
+++ geospatial_hurricane_analysis.py
@@ -1,12 +1,85 @@
-import streamlit as st
+#import streamlit as st
 import pandas as pd
 import numpy as np
 import json
 
 import datetime
 
-from streamlit_folium import folium_static
-import folium
+import map_builder
 
-from streamlit_timeline import timeline
+#from streamlit_folium import folium_static
+import folium
+import geopandas as gpd
+
+
+
+list_of_states_to_load = ["Virginia", "South Carolina", "North Carolina", "Georgia", "Alabama"] # , "Tennessee"
+
+strJSON_gdrive_path =  "./data/geo/georef-united-states-of-america-county.geojson"
+
+
+states = {"AL":"Alabama", "AK":"Alaska", "AZ":"Arizona", "AR":"Arkansas", "CA":"California", "CO":"Colorado", "CT":"Connecticut", 
+          "DC":"Washington DC", "DE":"Delaware", "FL":"Florida", "GA":"Georgia", "HI":"Hawaii", "ID":"Idaho", "IL":"Illinois", 
+          "IN":"Indiana", "IA":"Iowa", "KS":"Kansas", "KY":"Kentucky", "LA":"Louisiana", "ME":"Maine", "MD":"Maryland",
+          "MA":"Massachusetts", "MI":"Michigan", "MN":"Minnesota", "MS":"Mississippi", "MO":"Missouri", "MT":"Montana",
+          "NE":"Nebraska", "NV":"Nevada", "NH":"New Hampshire", "NJ":"New Jersey", "NM":"New Mexico", "NY":"New York", 
+          "NC":"North Carolina", "ND":"North Dakota", "OH":"Ohio", "OK":"Oklahoma", "OR":"Oregon", "PA":"Pennsylvania", 
+          "RI":"Rhode Island", "SC":"South Carolina", "SD":"South Dakota", "TN":"Tennessee", "TX":"Texas", "UT":"Utah", "VT":"Vermont",
+          "VA":"Virginia", "WA":"Washington", "WV":"West Virginia","WI":"Wisconsin", "WY":"Wyoming"}
+
+
+
+
+print("\n\nTop of Python Script")
+
+# st.set_page_config(layout="wide")
+
+# st.header('Geospatial Hurricane Analysis')
+
+# hurricane_select_col, top_map_col = st.columns([1,2])
+
+
+
+
+
+print("loading dataframe data...")
+# data load
+# df_joined_fema_data = pd.read_csv( "./data/snowflake_joined_fema_data.csv")
+
+
+# load geojson data for county lines
+geojson_online = gpd.read_file(strJSON_gdrive_path)
+
+
+
+geojson_online['coty_code'] = geojson_online['coty_code'].astype(str)
+geojson_online['coty_code'] = geojson_online['coty_code'].str.strip()
+
+geojson_online['ste_name'] = geojson_online['ste_name'].astype(str)
+geojson_online['ste_name'] = geojson_online['ste_name'].str.strip()
+
+df_geo = geojson_online[~geojson_online['geometry'].isna()]
+
+
+print( geojson_online.head() )
+
+# now load the list of extracted hurricane data
+
+
+df_hurricane_all_data_counties = pd.read_csv( "./data/hurricanes/2018_Michael_all_raw_events_counties.csv" )
+df_hurricane_all_data_counties = df_hurricane_all_data_counties.loc[:, ~df_hurricane_all_data_counties.columns.str.contains('^Unnamed')]
+
+
+df_hurricane_all_data_counties["fips"] = df_hurricane_all_data_counties["fips"].map(str)
+df_hurricane_all_data_counties["fips"] = df_hurricane_all_data_counties["fips"].str.zfill(5)
+
+print( df_hurricane_all_data_counties.head() )
+
+df_geo_storm = df_geo.merge(df_hurricane_all_data_counties, how="left", right_on=["fips"], left_on=["coty_code"])
+
+df_geo_area_to_render = df_geo_storm[ df_geo_storm["ste_name"].isin(list_of_states_to_load) ]
+
+
+
+map_builder.generate_folium_map(None, df_geo_area_to_render)
 
---
+++ map_builder.py
@@ -0,0 +1,281 @@
+import pandas as pd
+import numpy as np
+import time
+
+
+import folium
+import branca
+
+
+def generate_folium_map(df_fema=None, df_geo_area_to_render=None):
+
+	t0 = time.time()
+
+	print("building folium map...")
+
+	# create the map object
+
+	us_map_geojson = folium.Map(location=[40.7128, -74], zoom_start=6,tiles=None)
+	folium.TileLayer('CartoDB positron',name="Light Map",control=False).add_to(us_map_geojson) # CartoDB positron'
+
+
+
+
+	# build the feature groups
+
+	storm_name = "Michael"
+	storm_year = "2018"
+
+
+	feature_group_storm_path = folium.FeatureGroup(name=(storm_name + ' ' + storm_year + ' Storm Path'),overlay=True).add_to(us_map_geojson)
+
+	feature_group_storm_distance = folium.FeatureGroup(name=(storm_name + ' ' + storm_year + ' Storm County Distance'),overlay=False).add_to(us_map_geojson)
+
+	feature_group_storm_wind_vmax_sustained = folium.FeatureGroup(name=(storm_name + ' ' + storm_year + ' VMAX Sustained'),overlay=False).add_to(us_map_geojson)
+	feature_group_storm_wind_vmax_gusts = folium.FeatureGroup(name=(storm_name + ' ' + storm_year + ' VMAX Gusts'),overlay=False).add_to(us_map_geojson)
+
+	feature_group_storm_wind_sustained_duration = folium.FeatureGroup(name=(storm_name + ' ' + storm_year + ' Sustained Duration'),overlay=False).add_to(us_map_geojson)
+	feature_group_storm_wind_gust_duration = folium.FeatureGroup(name=(storm_name + ' ' + storm_year + ' Gust Duration'),overlay=False).add_to(us_map_geojson)
+
+	feature_group_storm_flood_exposure = folium.FeatureGroup(name=(storm_name + ' ' + storm_year + ' Flood Exposure'),overlay=False).add_to(us_map_geojson)
+
+
+
+
+
+	# build some layers
+
+	# 1. build the storm distance layer
+
+
+	storm_distance_colormap = branca.colormap.LinearColormap(colors=['#003302', '#237526', '#ffffff'], index=[10, 50, 175],vmin=0,vmax=400)
+
+	def storm_distance_layer_style_function(feature):
+		storm_value = feature["properties"]['storm_dist'] #.isnull()
+
+		if (feature["properties"]['storm_dist'] is None):
+
+			return {
+				'fillColor': '#eeeeee', 
+				'color':'#000000', 
+				'fillOpacity': 0.4, 
+				'weight': 0.1
+			}
+		else:
+			# nothing really
+			a=0
+
+		float_value = float(storm_value)
+
+
+		return {
+			'fillColor': """{fv}""".format( fv = storm_distance_colormap.rgb_hex_str(float_value) ), 
+			'color':'#000000', 
+			'fillOpacity': 0.8, 
+			'weight': 0.1
+		}
+
+	storm_dist_highlight_function = lambda x: {'fillColor': '#000000', 
+		'color':'#000000', 
+		'fillOpacity': 0.50, 
+		'weight': 0.1
+		}
+
+
+	storm_dist_base_layer = folium.features.GeoJson(
+		data=df_geo_area_to_render,
+		style_function=storm_distance_layer_style_function, 
+		control=True,
+		highlight_function=storm_dist_highlight_function, 
+		smooth_factor=0.1,
+		tooltip=folium.features.GeoJsonTooltip(
+			fields=['coty_name_long', 'coty_code','storm_dist'],
+			aliases=['County: ', 'FIPS: ','Storm Distance: '],
+			style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
+		)
+	)
+
+	feature_group_storm_distance.add_child( storm_dist_base_layer )	
+
+	# 2. build the [ Wind VMAX Sustained ] layer
+
+
+	storm_vmax_sustained_layer_colormap = branca.colormap.LinearColormap(colors=['#ffffff', '#ff0000', '#630000'], index=[14, 25, 35],vmin=0,vmax=35)
+
+	def storm_vmax_sustained_layer_style_function(feature):
+		storm_value = feature["properties"]['vmax_sust'] #.isnull()
+
+	  #print(pre_storm_avg_admissions)
+
+		if (feature["properties"]['vmax_sust'] is None):
+
+			return {
+				'fillColor': '#eeeeee', 
+				'color':'#000000', 
+				'fillOpacity': 0.4, 
+				'weight': 0.1
+				}
+		else:
+			# nothing really
+			a=0
+
+		float_value = float(storm_value)
+
+
+		return {
+			'fillColor': f'{storm_vmax_sustained_layer_colormap.rgb_hex_str(float_value)}', 
+			'color':'#000000', 
+			'fillOpacity': 0.8, 
+			'weight': 0.1
+			}
+
+	storm_vmax_sustained_highlight_function = lambda x: {'fillColor': '#000000', 
+		'color':'#000000', 
+		'fillOpacity': 0.50, 
+		'weight': 0.1}
+
+
+	storm_vmax_sustained_layer = folium.features.GeoJson(
+		data=df_geo_area_to_render,
+		style_function=storm_vmax_sustained_layer_style_function, 
+		control=True,
+		highlight_function=storm_vmax_sustained_highlight_function, 
+		smooth_factor=0.1,
+		tooltip=folium.features.GeoJsonTooltip(
+			fields=['coty_name_long', 'coty_code','vmax_sust'],
+			aliases=['County: ', 'FIPS: ','VMAX Sustained: '],
+			style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
+		)
+	)
+
+	feature_group_storm_wind_vmax_sustained.add_child( storm_vmax_sustained_layer )
+
+
+
+
+
+	# 3. build the [ vmax wind gusts ] layer
+
+
+	# feature_group_storm_wind_vmax_gusts
+
+
+	storm_vmax_gusts_layer_colormap = branca.colormap.LinearColormap(colors=['#ffffff', '#ff0000', '#630000'], index=[15, 35, 55],vmin=0,vmax=55)
+
+	def storm_vmax_gusts_layer_style_function(feature):
+		storm_value = feature["properties"]['vmax_gust'] #.isnull()
+
+		if (feature["properties"]['vmax_gust'] is None):
+
+			return {
+				'fillColor': '#eeeeee', 
+				'color':'#000000', 
+				'fillOpacity': 0.4, 
+				'weight': 0.1
+			}
+		else:
+			# nothing really
+			a=0
+
+		float_value = float(storm_value)
+
+		return {
+			'fillColor': f'{storm_vmax_gusts_layer_colormap.rgb_hex_str(float_value)}', 
+			'color':'#000000', 
+			'fillOpacity': 0.8, 
+			'weight': 0.1
+		}
+
+	storm_vmax_gusts_highlight_function = lambda x: {'fillColor': '#000000', 
+		'color':'#000000', 
+		'fillOpacity': 0.50, 
+		'weight': 0.1}
+
+
+	storm_vmax_GUSTS_layer = folium.features.GeoJson(
+		data=df_geo_area_to_render,
+		style_function=storm_vmax_gusts_layer_style_function, 
+		control=True,
+		highlight_function=storm_vmax_gusts_highlight_function, 
+		smooth_factor=0.1,
+		tooltip=folium.features.GeoJsonTooltip(
+			fields=['coty_name_long', 'coty_code','vmax_gust'],
+			aliases=['County: ', 'FIPS: ','VMAX Gusts: '],
+			style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
+		)
+	)
+
+	feature_group_storm_wind_vmax_gusts.add_child( storm_vmax_GUSTS_layer )
+
+
+
+
+
+
+	# N. build the [ Flood Exposure ] layer
+
+
+	storm_flood_layer_colormap = branca.colormap.LinearColormap(colors=['#ffffff', '#0000ff'], index=[0, 1],vmin=0,vmax=1)
+
+	def storm_flood_layer_style_function(feature):
+		storm_value = feature["properties"]['flood_exposure'] #.isnull()
+
+		if ((feature["properties"]['flood_exposure'] is None) | (feature["properties"]['flood_exposure'] == False)):
+
+			return {
+			'fillColor': '#eeeeee', 
+			'color':'#000000', 
+			'fillOpacity': 0.4, 
+			'weight': 0.1
+			}
+		else:
+			# nothing really
+			a=0
+
+		float_value = float(storm_value)
+
+		return {
+			'fillColor': f'{storm_flood_layer_colormap.rgb_hex_str(1)}', 
+			'color':'#000000', 
+			'fillOpacity': 0.4, 
+			'weight': 0.1
+		}
+
+	storm_flood_highlight_function = lambda x: {'fillColor': '#000000', 
+		'color':'#000000', 
+		'fillOpacity': 0.50, 
+		'weight': 0.1}
+
+
+	storm_flood_layer = folium.features.GeoJson(
+		data=df_geo_area_to_render,
+		style_function=storm_flood_layer_style_function, 
+		control=True,
+		highlight_function=storm_flood_highlight_function, 
+		smooth_factor=0.1,
+		tooltip=folium.features.GeoJsonTooltip(
+			fields=['coty_name_long', 'coty_code','flood_exposure'],
+			aliases=['County: ', 'FIPS: ','Flood Exposure: '],
+			style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
+		)
+	)
+
+	feature_group_storm_flood_exposure.add_child( storm_flood_layer )	
+
+
+
+	# save the map
+
+	folium.LayerControl().add_to(us_map_geojson)
+	us_map_geojson.save(outfile = "./maps/test/test_map.html")
+
+	t1 = time.time()
+
+	total = t1-t0
+
+	print("Map Generation Time: " + str(total))
+
+
+
+
+	return 0
